c04e4a0d7b2b27bb4d19e4e2b2b0cb9cf41932cb
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2020-12-21 10:38:13 +0700

    Add project files.

---
+++ ServerConfigRest_20180140119_ArifBudimanArrosyid/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceRest_20180140119_ArifBudimanArrosyid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_20180140119_ArifBudimanArrosyid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
